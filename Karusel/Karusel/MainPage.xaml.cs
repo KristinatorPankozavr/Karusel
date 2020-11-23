42542af022a6c6074f5ce80f65c9123413d6dfc7
Kristina Pankova <Kristina Pankova@WIN-CUR9MCP5QTE>
2020-11-23 18:57:43 +0200

    Добавьте файлы проекта.

---
+++ Karusel/Karusel/MainPage.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Karusel
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            var start = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                         new Frame
+                        {
+                            BackgroundColor = Color.Wheat,
+                            Content = new Label { Text = "Виды темперамента", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center}
+
+                        },
+                          new Image {Source = "es.jpg",
+                          WidthRequest = 10,
+                          HeightRequest = 400,},
+                        new Label
+                        {
+                            Text = "Темперамент- Совокупность душевных, психических свойств человека, характеризующих степень его возбудимости и проявляющихся в его отношении к окружающей действительности, в поведении.",
+                            HorizontalOptions = LayoutOptions.Center,
+                            TextColor = Color.Black,
+                            BackgroundColor = Color.Wheat,
+                            WidthRequest = 300,
+                            HeightRequest = 250,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                        },
+                    }
+                }
+            };
+            var holerik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Frame
+                        {
+                            BackgroundColor = Color.Wheat,
+                            Content = new Label { Text = "Холерик", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                             HorizontalOptions = LayoutOptions.Center,}
+
+                        },
+                        new Label
+                        {
+                            Text = "Импульсивность, агрессия",
+                            TextColor = Color.Black,
+                            BackgroundColor = Color.Wheat,
+                            HorizontalOptions = LayoutOptions.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                        },
+                        new Image {Source = "zlo.jpg",
+                          WidthRequest = 10,
+                          HeightRequest = 400,}
+                    }
+                }
+            };
+            var sangvinik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Frame
+                        {
+                            BackgroundColor = Color.Wheat,
+                            Content = new Label { Text = "Сангвиник", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                             HorizontalOptions = LayoutOptions.Center,}
+
+                        },
+                        new Label
+                        {
+                            Text = "Подвижный, социальный, уверенный в себе",
+                            TextColor = Color.Black,
+                            BackgroundColor = Color.Wheat,
+                            HorizontalOptions = LayoutOptions.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                        },
+                        new Image {Source = "vloroee.jpg",
+                          WidthRequest = 10,
+                          HeightRequest = 400,}
+                    }
+                }
+            };
+            var flegmatik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                         new Frame
+                        {
+                            BackgroundColor = Color.Wheat,
+                            Content = new Label { Text = "Флегматик", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                             HorizontalOptions = LayoutOptions.Center,}
+
+                        },
+                        new Label
+                        {
+                            Text = "Социально отстраненный, погруженный в себя",
+                            TextColor = Color.Black,
+                            BackgroundColor = Color.Wheat,
+                            HorizontalOptions = LayoutOptions.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                        },
+                        new Image {Source = "fleg.jpg",
+                          WidthRequest = 10,
+                          HeightRequest = 400,}
+                    }
+                }
+            };
+            var melanholik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                         new Frame
+                        {
+                            BackgroundColor = Color.Wheat,
+                            Content = new Label { Text = "Меланхолик", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                             HorizontalOptions = LayoutOptions.Center,}
+
+                        },
+                        new Label
+                        {
+                            Text = "Грустный, боязливый, подавленный, слабый",
+                            TextColor = Color.Black,
+                            BackgroundColor = Color.Wheat,
+                            HorizontalOptions = LayoutOptions.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                        },
+                        new Image {Source = "grust.jpg",  
+                            WidthRequest = 300,
+                            HeightRequest = 300,}
+                    }
+                }
+            };
+            Children.Add(start);
+            Children.Add(holerik);
+            Children.Add(sangvinik);
+            Children.Add(flegmatik);
+            Children.Add(melanholik);
+        }
+    }
+}
